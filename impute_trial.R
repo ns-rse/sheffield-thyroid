8cdc7afd5cfd0931f50a159fe7ca6fe83b4ce62e
Ovie Edafe <o.edafe@sheffield.ac.uk>
2024-08-10 18:52:49 +0100

    lines to visualise tsh original versus imputations

--- impute_trial.R
+++ impute_trial.R
@@ -4,10 +4,11 @@ attributes(df_imp)
 
 ## complete
 library(VIM)
-df_imp <- complete(df_imp)
-aggr(df_imp)
-view(df_imp)
+df_trial <- complete(df_imp)
+aggr(df_trial)
+view(df_trial)
 boxplot(df$tsh_value)
-boxplot(df_imp$tsh_value)
-t.test(df$tsh_value, df_imp$tsh_value)
-       
+boxplot(df_trial$tsh_value)
+t.test(df$tsh_value, df_trial$tsh_value)
+plot(density(df$tsh_value, na.rm = TRUE), main = "Data wit NA")
+lines(density(df_trial$tsh_value, na.rm = TRUE), col = "red", lty = 3)
