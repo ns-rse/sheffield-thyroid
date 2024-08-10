95e91865abdd27bf0d154f4623481e1f8e5e6eb3
Ovie Edafe <o.edafe@sheffield.ac.uk>
2024-08-10 18:37:13 +0100

    rough trial of imputation using default settings

---
+++ impute_trial.R
@@ -0,0 +1,13 @@
+## mice imputation trials
+df_imp <- mice(df, printFlag = F, seed = 123)
+attributes(df_imp)
+
+## complete
+library(VIM)
+df_imp <- complete(df_imp)
+aggr(df_imp)
+view(df_imp)
+boxplot(df$tsh_value)
+boxplot(df_imp$tsh_value)
+t.test(df$tsh_value, df_imp$tsh_value)
+        
